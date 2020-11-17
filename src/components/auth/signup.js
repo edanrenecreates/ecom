e6a1cadcc75f0b02a034b90c756ed344590adf70
edanrenecreates <rene@edanramos.com>
2020-11-16 20:29:59 -0700

    developed sign up form and modified formbutton to have short gray case

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 
-class SignUp extends Component{
+import SignUpForm from './signupForm';
+import PageTitle from '../pageTitle';
+
+class SignUp extends Component {
+
+  onSubmit = (fields) => {
+    console.log(fields);
+  }
   render() {
-    return(
+    return (
       <div className='sign-up'>
-        Sign Up!
+        <PageTitle className='sign-up__page-title' title="Register"/>
+        <SignUpForm onSubmit={this.onSubmit} className='sign-up__form'/>
       </div>
     )
   }
 }
+
 
 export default SignUp;
