80652cbbad7a92c9535bb024e08d4431e4189a47
edanrenecreates <rene@edanramos.com>
2020-11-23 19:08:50 -0700

    developed shopReducer and fetch shop categories, action creator

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -14,6 +14,7 @@ class Shop extends Component {
       }
     ]
     this.props.setHeaderLinks(headerLinks);
+    this.props.fetchShopCategories();
     // fetch the navbar links 
       // set the navbar links
       // filter products with links
