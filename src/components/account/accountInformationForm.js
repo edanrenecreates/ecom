9baa3f0c78f92a93dc758c68382352c549328ce8
edanrenecreates <rene@edanramos.com>
2020-11-23 18:18:30 -0700

    finished account information form

--- src/components/account/accountInformationForm.js
+++ src/components/account/accountInformationForm.js
@@ -26,37 +26,37 @@ class AccountInformationForm extends Component {
           title='Name'
           placeholder='Name'
           name='name'
-          component={FormInput} />
+          component={FormInput}/>
         <Field className='account-information-form__email'
           type='email'
           title='Email'
           placeholder='Email'
           name='email'
-          component={FormInput} />
+          component={FormInput}/>
         <Field className='account-information-form__street-address'
           type='address'
           title='Street Address'
           placeholder='Street Address'
           name='address'
-          component={FormInput} />
+          component={FormInput}/>
         <Field className='account-information-form__city'
           type='city'
           title='City'
           placeholder='City'
           name='city'
-          component={FormInput} />
+          component={FormInput}/>
         <Field className='account-information-form__state'
           type='state'
           title='State'
           placeholder='State'
           name='state'
-          component={FormInput} />
+          component={FormInput}/>
         <Field className='account-information-form__zipcode'
           type='zipcode'
           title='Zipcode'
           placeholder='Zipcode'
           name='zipcode'
-          component={FormInput} />
+          component={FormInput}/>
 
         <Field className='account-information-form__change-password'
           onClick={() => console.log('trying to show passwords')}
@@ -64,7 +64,7 @@ class AccountInformationForm extends Component {
           labelTitle='Password'
           title='Change Password'
           name='change-password'
-          component={LongGrayButton} />
+          component={LongGrayButton}/>
         {
           this.state.showPasswords ?
             [
@@ -73,19 +73,33 @@ class AccountInformationForm extends Component {
                 title='Current Password'
                 placeholder='Current Password'
                 name='current'
-                component={FormInput} />,
+                component={FormInput}/>,
               <Field key={1} className='account-information-form__new'
                 type='password'
                 title='New Password'
                 placeholder='New Password'
                 name='new'
-                component={FormInput} />,
-              <Field key={2}className='account-information-form__confirm'
+                component={FormInput}/>,
+              <Field key={2} className='account-information-form__confirm'
                 type='password'
                 title='Confirm Password'
                 placeholder='Confirm Password'
                 name='confirm'
-                component={FormInput} />
+                component={FormInput}/>,
+              <div key={3} className='account-information-form__line'></div>,
+              <Field key={4} className='account-information-form__update-information'
+                onClick={() => this.setState({ showPasswords: false })}
+                type='submit'
+                title='Update Information'
+                name='update-information'
+                component={FormButton}/>,
+              <Field key={5} className='account-information-form__cancel'
+                onClick={() => this.setState({ showPasswords: false })}
+                type='button'
+                title='Cancel'
+                name='cancel'
+                short={true}
+                component={FormButton}/>
             ]
             :
             <Field className='account-information-form__change-password'
@@ -94,7 +108,7 @@ class AccountInformationForm extends Component {
               labelTitle='Password'
               title='Change Password'
               name='change-password'
-              component={LongGrayButton} />
+              component={LongGrayButton}/>
         }
       </form>
     )
